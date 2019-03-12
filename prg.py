e6610911603bdab0c37bb3b0a87d73d5541c460b
Lalit Sharma <lalits429@gmail.com>
2019-03-12 21:48:39 +0530

    Update prg.py

--- prg.py
+++ prg.py
@@ -2,3 +2,12 @@
 
 def f():
     print("hello world")
+    def number(num1,num2,num3):
+    if num1 >=num2 and num1 >=num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+print(number(100,100,100))
